4857fdae860f0139643014fafc6f8795e1d8babc
Kareshin <56518435+Kareshin@users.noreply.github.com>
2023-05-09 20:32:53 +0200

    Update zadatak_2.py

--- OSU_MK_LV2/zadatak_2.py
+++ OSU_MK_LV2/zadatak_2.py
@@ -5,12 +5,14 @@ arr=np.genfromtxt("data.csv",delimiter=",")
 
 arr=np.delete(arr,0,axis=0)
 
-
+#a
 rows, cols=np.shape(arr)
 print("Mjerenja su izvrsena na "+str(rows)+" osoba")
 
-x=arr[:,1] #height
-y=arr[:,2] #weight
+#b
+
+x=arr[:,1] #height (znaci u x se sprema samo vrijednosti prvog stupca)
+y=arr[:,2] #weight (znaci u y se sprema samo vrijednosti drugog stupca)
 
 
 plt.scatter(x,y)
@@ -19,7 +21,8 @@ plt.ylabel("Masa")
 plt.title("Odnos visine i mase")
 plt.show()
 
-x5=x[::50]
+#c
+x5=x[::50] #korak 50, svaki 50-ti
 y5=y[::50]
 
 plt.scatter(x5,y5)
@@ -28,14 +31,15 @@ plt.ylabel("Masa")
 plt.title("Odnos visine i mase svakih 50 osoba")
 plt.show()
 
+#d
 print("Minimalna vrijednost visine je: "+str(np.min(x)))
 print("Maksimalna vrijednost visine je: "+str(np.max(x)))
 print("Srednja vrijednost visine je: "+str(np.mean(x)))
 
-
+#e
 male = []
 female = []
-for people in data[::50]:
+for people in arr[::50]:
     if people[0] == 1:
         male.append(people[1])
     else:
