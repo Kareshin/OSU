43ca0321b9652c56822c28f21056496e17545143
Kareshin <56518435+Kareshin@users.noreply.github.com>
2023-05-09 09:24:57 +0200

    Update treci.py

--- OSU_MK_LV1/treci.py
+++ OSU_MK_LV1/treci.py
@@ -1,21 +1,17 @@
-numbers = []
-while True:
-    number = input("Unesi broj: ")
-    if number == "Done":
+list = []
+while 1:
+    word = input()
+    if word.isdigit():
+        list.append(int(word))
+    elif word == "Done":
         break
-    try:
-        number = float(number)
-    except:
-        print("Unesena vrijednost nije broj niti Done")
-    numbers.append(number)
+    else:
+        print("NaN")
+        continue
     
-
-if len(numbers)==0:
-    print("Lista je prazna")
-else:
-    numbers = sorted(numbers)
-    print(f"Uneseno brojeva: {len(numbers)}")
-    print(f"Sortirana lista: {numbers}")
-    print(f"Minimalna vrijednost u listi: {min(numbers)}")
-    print(f"Maksimalna vrijednost u listi: {max(numbers)}")
-    print(f"Srednja vrijednost: {sum(numbers)/len(numbers)}")
+numbers = sorted(list)
+print(f"Uneseno brojeva: {len(numbers)}")
+print(f"Sortirana lista: {numbers}")
+print(f"Minimalna vrijednost u listi: {min(numbers)}")
+print(f"Maksimalna vrijednost u listi: {max(numbers)}")
+print(f"Srednja vrijednost: {sum(numbers)/len(numbers)}")
