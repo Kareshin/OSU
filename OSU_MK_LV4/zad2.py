94cd6e55762dc1bf479d15f53c4907f2463d06ca
Kareshin <56518435+Kareshin@users.noreply.github.com>
2023-05-10 18:27:33 +0200

    Update zad2.py

--- OSU_MK_LV4/zad2.py
+++ OSU_MK_LV4/zad2.py
@@ -1,61 +1,76 @@
-import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from sklearn import datasets
+
+from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import OneHotEncoder
-import sklearn.linear_model as lm
-from sklearn import metrics
 
-#a
-data = pd.read_csv('data_C02_emission.csv')
-
-input_variables = ['Fuel Consumption City (L/100km)','Fuel Consumption Hwy (L/100km)',
-                   'Fuel Consumption Comb (L/100km)','Fuel Consumption Comb (mpg)',
-                   'Engine Size (L)','Cylinders','Fuel Type']
-
-output_variables = ['CO2 Emissions (g/km)']
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+from sklearn.metrics import confusion_matrix , ConfusionMatrixDisplay
 
 
-# koristimo i kategoricku varijablu 'Fuel Type' pa ju moramo 1-od-K kodirat
-ohe = OneHotEncoder ()
-X_encoded = ohe.fit_transform ( data[['Fuel Type']]) . toarray ()
-data['Fuel Type'] = X_encoded
+X, y = make_classification(n_samples=200, n_features=2, n_redundant=0, n_informative=2,
+                            random_state=213, n_clusters_per_class=1, class_sep=1)
 
-X = data[input_variables].to_numpy()
-y = data[output_variables].to_numpy()
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
-X_train , X_test , y_train , y_test = train_test_split (X , y , test_size = 0.2 , random_state =1 )
+# a
+plt.figure()
+customCmap = matplotlib.colors.ListedColormap(["red", "blue"])
+plt.scatter(X_train[:,0],X_train[:,1],c=y_train,cmap=customCmap)
+plt.scatter(X_test[:,0],X_test[:,1],c=y_test,cmap=customCmap,marker='x')
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.title("Podaci za treniranje(.) i testiranje(x)")
+plt.legend(["x*2" , "x*3"])
 
-#b
-plt.scatter(X_train[:,0],y_train,c='blue',s=2)
-plt.scatter(X_test[:,0],y_test,c='red',s=2)
+
+# b
+LogRegression_model = LogisticRegression ()
+LogRegression_model.fit( X_train , y_train )
+
+# c
+# u .coef_ se nalaze fi1 i fi2 dok je fi0 zapisan u .intercept_ jer oznacava presjek s y-osi
+print(LogRegression_model.coef_)
+th = LogRegression_model.coef_[0]
+
+# granica odluke definirana kao x2 = -x1*fi1/fi2 - fi0/fi2
+a = -th[0] / th[1]
+x1 = np.linspace(-5, 5)
+
+x2 = a * x1 - (LogRegression_model.intercept_[0]) / th[1]
+
+plt.plot(x1, x2, 'k-')
 plt.show()
 
+#d
+y_pred = LogRegression_model . predict ( X_test )
 
+cm = confusion_matrix( y_test , y_pred )
+print (" Matrica zabune:\n" , cm )
+disp = ConfusionMatrixDisplay ( confusion_matrix ( y_test , y_pred ) )
+disp.plot ()
+plt.show ()
 
-# d
-linearModel = lm.LinearRegression ()
-linearModel.fit( X_train , y_train )
+print("Accuracy: " , accuracy_score(y_test , y_pred ))
+print("Precision: ", precision_score(y_test , y_pred ))
+print("Recall: ", recall_score(y_test , y_pred))
 
-print(linearModel.coef_)
 
 # e
-y_test_p = linearModel . predict ( X_test)
-plt.scatter(y_test, y_test_p, s=2)
+
+customCmap = matplotlib.colors.ListedColormap(["red", "blue"])
+customCmap2 = matplotlib.colors.ListedColormap(["orange", "green"]) #Black and Green are too similar
+y_isTrue = np.arange(len(y_test))
+
+for i in range(len(y_test)):
+    if(y_test[i] == y_pred[i]):
+        y_isTrue[i] = 1
+    else:
+        y_isTrue[i] = 0
+
+plt.figure()
+plt.scatter(X_test[:,0],X_test[:,1],c=y_isTrue,cmap=customCmap)
 plt.show()
-
-# f
-MSE = metrics.r2_score(y_test, y_test_p)
-print(MSE)
-
-# e
-# Povecavanjem broja ulaznih podataka imamo bolje učenje, tj. r2_score je blize 1
-
-# 2zad
-# kolika je maksimalna pogreška u procjeni CO2 plinova
-errArray = abs(y_test_p - y_test)
-id = np.argmax(errArray)
-print(errArray[id,0])
-print(data['Model'].to_numpy()[id])
