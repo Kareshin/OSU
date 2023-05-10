afb7f6b7772c196a3b654c38792e52bd92611951
Kareshin <56518435+Kareshin@users.noreply.github.com>
2023-05-10 15:13:32 +0200

    Update zad2.py

--- OSU_MK_LV4/zad2.py
+++ OSU_MK_LV4/zad2.py
@@ -1,34 +1,61 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-import sklearn.linear_model as lm
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_percentage_error
-from sklearn.metrics import r2_score
-from sklearn.metrics import max_error
-from sklearn.preprocessing import OneHotEncoder
+from sklearn import datasets
+from sklearn . model_selection import train_test_split
+from sklearn . preprocessing import MinMaxScaler
+from sklearn . preprocessing import OneHotEncoder
+import sklearn . linear_model as lm
+from sklearn import metrics
 
-data = pd.read_csv('data_C02_emission.csv')
-X = data[['Engine Size (L)', 'Cylinders', 'Fuel Type', 'Fuel Consumption City (L/100km)',
-          'Fuel Consumption Hwy (L/100km)', 'Fuel Consumption Comb (L/100km)', 'Fuel Consumption Comb (mpg)']]
-y = data['CO2 Emissions (g/km)'].copy()
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1)
-ohe = OneHotEncoder()
-X_encoded = ohe.fit_transform(data[['Fuel Type']]).toarray()
-linearModel = lm.LinearRegression()
-linearModel.fit(X_encoded, y_train)
-y_prediction = linearModel.predict(X_test)
-print("Mean squared error: ", mean_squared_error(y_test, y_prediction))
-print("Mean absolute error: ", mean_absolute_error(y_test, y_prediction))
-print("Mean absolute percentage error: ",
-      mean_absolute_percentage_error(y_test, y_prediction), "%")
-print("R2 score: ", r2_score(y_test, y_prediction))
+#a
+data = pd . read_csv('data_C02_emission.csv')
 
-print("Max error: ", max_error(y_test, y_prediction))
-error = abs(y_prediction - y_test)
-id = np.argmax(error)
-print(data[id]['Make'], ['Model'])
+input_variables = ['Fuel Consumption City (L/100km)','Fuel Consumption Hwy (L/100km)',
+                   'Fuel Consumption Comb (L/100km)','Fuel Consumption Comb (mpg)',
+                   'Engine Size (L)','Cylinders','Fuel Type']
+
+output_variables = ['CO2 Emissions (g/km)']
+
+
+# koristimo i kategoricku varijablu 'Fuel Type' pa ju moramo 1-od-K kodirat
+ohe = OneHotEncoder ()
+X_encoded = ohe . fit_transform ( data[['Fuel Type']]) . toarray ()
+data['Fuel Type'] = X_encoded
+
+X = data[input_variables].to_numpy()
+y = data[output_variables].to_numpy()
+
+X_train , X_test , y_train , y_test = train_test_split (X , y , test_size = 0.2 , random_state =1 )
+
+#b
+plt.scatter(X_train[:,0],y_train,c='blue',s=2)
+plt.scatter(X_test[:,0],y_test,c='red',s=2)
+plt.show()
+
+
+
+# d
+linearModel = lm . LinearRegression ()
+linearModel . fit ( X_train , y_train )
+
+print(linearModel.coef_)
+
+# e
+y_test_p = linearModel . predict ( X_test)
+plt.scatter(y_test, y_test_p, s=2)
+plt.show()
+
+# f
+MSE = metrics.r2_score(y_test, y_test_p)
+print(MSE)
+
+# e
+# Povecavanjem broja ulaznih podataka imamo bolje učenje, tj. r2_score je blize 1
+
+# 2zad
+# kolika je maksimalna pogreška u procjeni CO2 plinova
+errArray = abs(y_test_p - y_test)
+id = np.argmax(errArray)
+print(errArray[id,0])
+print(data['Model'].to_numpy()[id])
